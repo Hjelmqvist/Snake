f505b7e020d84f93ab58e68bbe5d981a9ed25d70
Hjelmqvist <mathias.hjelmqvist@futuregames.nu>
2021-11-15 10:51:03 +0100

    Removed profiling

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Hjelmqvist.Pathfinding;
-using UnityEngine.Profiling;
 
 public class AIController : MonoBehaviour
 {
@@ -22,16 +21,13 @@ public class AIController : MonoBehaviour
 
             Tile currentTile = _grid.GetTile( _snake.CurrentPosition );
 
-            Profiler.BeginSample( "AStar pathfinding" );
             if (Pathfinding.AStar.TryGetPath( currentTile, _fruitManager.FruitTile, out List<Vector2Int> path ))
             {
-                Profiler.EndSample();
                 if (path.Count > 1)
                     _snake.ChangeDirection( path[1] - path[0] );
             }
             else
             {
-                Profiler.EndSample();
                 // Move to any connecting walkable tile
                 foreach (IPathable pathable in currentTile.Connections)
                 {
